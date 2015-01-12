aa81f870d17e23605d438d484d234e2b3ba4f3c8
Lonnie VanZandt <lonniev@gmail.com>
2015-01-12 13:39:37 -0700

    Initial commit.
    Small Chef recipe to install and eventually configure Eclipse on
    Windows hosts with Chocolatey.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Description
+- Installs the Eclipse IDE
+
+# Requirements
+- java cookbook
+- chocolatey cookbook
+
+# Platform
+- Windows
+
+# Usage
+ 
+add_recipe "eclipse-windows"
+
+# Attributes
+
+None
+
+# Author
+
+Author:: Lonnie VanZandt, lonniev@gmail.com
---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'eclipse-windows'
+maintainer       'Lonnie VanZandt'
+maintainer_email 'lonniev@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures eclipse for Windows'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          "java"
+depends          "chocolatey"
